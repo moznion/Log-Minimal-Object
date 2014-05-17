fb1844738a61fefbd9d813fadf36ca5214d055c1
moznion <moznion@gmail.com>
2014-05-17 14:06:13 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Log::Minimal::Object - It's new $module
+
+# SYNOPSIS
+
+    use Log::Minimal::Object;
+
+# DESCRIPTION
+
+Log::Minimal::Object is ...
+
+# LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+moznion <moznion@gmail.com>
---
+++ lib/Log/Minimal/Object.pm
@@ -0,0 +1,39 @@
+package Log::Minimal::Object;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Log::Minimal::Object - It's new $module
+
+=head1 SYNOPSIS
+
+    use Log::Minimal::Object;
+
+=head1 DESCRIPTION
+
+Log::Minimal::Object is ...
+
+=head1 LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+moznion E<lt>moznion@gmail.comE<gt>
+
+=cut
+
